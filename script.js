731e07cadd7c7e5a9ed15f64893833d11c1cbcb3
Nithish12Krish <nithish1281999@gmail.com>
2020-08-21 13:25:01 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const videoElement=document.getElementById('video');
+const button=document.getElementById('button');
 
+//prompt to select media stream, pass to video element, then play
+async function selectedMediaStream()
+{
+    try{
+        const mediaStream=await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject=mediaStream;
+        videoElement.onloadedmetadata=()=>{
+            videoElement.play();
+        }
+    }catch(error){
+        //
+    }
+}
+button.addEventListener('click',async ()=>{
+  //disable button
+  button.disabled=true;
+  //start PIP
+  await videoElement.requestPictureInPicture();
+  //Reset button
+  button.disabled=false;
+});
+//onLoad
+selectedMediaStream();
